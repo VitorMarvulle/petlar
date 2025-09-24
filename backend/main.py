9a6dc348c66e367da017b46a10e475954fe308e9
odutra-dev <gabrieldutraamarante@gmail.com>
2025-09-23 22:34:39 -0300

    feat: add router get users and env

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,20 @@
-# backend/main.py (VERSÃO CORRETA)
-
 from fastapi import FastAPI
+import  requests
+from os import getenv
+from dotenv import load_dotenv
 
-# --- Esta é a linha que faltava ---
-# Aqui você cria a instância do FastAPI e a atribui à variável "app"
-# que o Uvicorn está procurando.
+load_dotenv()
+
+SUPABASE_URL = getenv('SUPABASE_URL')
+SUPABASE_KEY = getenv('SUPABASE_KEY')
 app = FastAPI()
 
-# Vamos criar uma rota de teste para ter certeza de que tudo funciona.
-# Esta é a rota raiz ("/").
-@app.get("/")
-def read_root():
-    # Esta função será executada quando alguém acessar a URL principal da API.
-    return {"message": "Bem-vindo à API do Lar Doce Pet!! Teste Docker!!!"}
-
-# Você pode manter seus prints para ver quando o módulo é carregado, se quiser.
-print("Módulo main.py carregado e instância FastAPI criada. ALOU")
+@app.get("/usuarios")
+def get_usuarios():
+    url = f"{SUPABASE_URL}/rest/v1/usuarios"
+    headers = {
+        "apikey":SUPABASE_KEY,
+        "Authorization": f"Bearer {SUPABASE_KEY}"
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
