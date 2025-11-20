24966ad649ad811dda4b9f7a6162cee624e6f5ba
odutra-dev <gabrieldutraamarante@gmail.com>
2025-11-20 15:49:42 -0300

    test: refactor tests intergrations user for 94%

--- backend/tests/intergrations/test_usuarios-intergration-test.py
+++ backend/tests/intergrations/test_usuarios-intergration-test.py
@@ -19,7 +19,7 @@ def test_get_usuarios():
     body = response.json()
     assert isinstance(body, list), "GET /usuarios deve retornar lista"
 
-
+    
 
 # ------------------------
 # POST /usuarios
@@ -65,7 +65,6 @@ def test_post_usuarios():
 
     global usuario_ID
     usuario_ID = data["id_usuario"]
-
 
 # ------------------------
 # GET /usuarios/{id}
@@ -127,3 +126,55 @@ def test_get_usuario_by_id_not_found():
     assert response.status_code == 404
     assert response.json()["detail"] == "Usuário não encontrado"
 
+
+# ------------------------
+# POST /usuarios - sem data_cadastro
+# ------------------------
+
+def test_post_usuarios_not_null_data_cadastro():
+
+    user = dict(UsuarioCreate(
+        nome="John Doe",
+        email=f"lKb8A{number_random}@example.com",
+        senha_hash="hashed_password",
+        telefone="1234567890",
+        tipo="tutor",
+        logradouro="Rua A",
+        numero="123",
+        bairro="Bairro X",
+        cidade="Cidade Y",
+        uf="UF",
+        cep="12345-678",
+        complemento="Complemento Z"
+    ))
+
+    response = client.post("/usuarios", json=user)
+    assert response.status_code == 201
+
+    body = response.json()
+    assert isinstance(body, list), "POST /usuarios retorna lista"
+
+    data = body[0]  # pega primeiro item da lista
+
+    assert data["nome"] == user["nome"]
+    assert data["email"] == user["email"]
+    assert data["telefone"] == user["telefone"]
+    assert data["tipo"] == user["tipo"]
+    assert data["logradouro"] == user["logradouro"]
+    assert data["numero"] == user["numero"]
+    assert data["bairro"] == user["bairro"]
+    assert data["cidade"] == user["cidade"]
+    assert data["uf"] == user["uf"]
+    assert data["cep"] == user["cep"]
+    assert data["complemento"] == user["complemento"]
+
+    assert data["data_cadastro"] != None
+
+    global usuario_ID
+    usuario_ID = data["id_usuario"]
+
+    # Deletar o usuário criado
+    response = client.delete(f"/usuarios/{usuario_ID}")
+    assert response.status_code in (200, 204), "DELETE deve retornar 200 ou 204"
+
+    
